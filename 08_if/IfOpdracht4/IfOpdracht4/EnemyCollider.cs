1e701713c25300af8bd3da0bdbcbb9cc8dbee5f3
Elitetykoro <tykoromathijssen@gmail.com>
2024-03-11 14:26:39 +0100

    les8

--- 08_if/IfOpdracht4/IfOpdracht4/EnemyCollider.cs
+++ 08_if/IfOpdracht4/IfOpdracht4/EnemyCollider.cs
@@ -4,15 +4,18 @@
     {
         internal bool CanPassWall(Enemy enemy)
         {
+            if(enemy.flying || enemy.subterranian) { return true; }
+            else { return false; }
             //gebruik hier een || (OR)
 			//je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
-            return false;//vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian) true is;
+            //vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian) true is;
         }
         internal bool CanPassWater(Enemy enemy)
         {
+            if (enemy.incoporial || enemy.swimmer) { return true; }
             //gebruik hier een || (OR)
-			//je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
-            return false;//vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian,swimmer) true is;
+            //je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+            else { return false; }//vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian,swimmer) true is;
         }
     }
 }
--- extra/Clicker/Clicker/ClickerForm.Designer.cs
+++ extra/Clicker/Clicker/ClickerForm.Designer.cs
@@ -45,6 +45,6 @@
 
         }
 
-        #endregion
+        #endregion;
     }
 }
